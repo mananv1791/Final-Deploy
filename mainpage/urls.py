ba5689b155439f4dbc749bcf922d78050b6621ef
Manan Parmar <54072015+mananv1791@users.noreply.github.com>
2021-06-24 21:28:57 +0530

    Update urls.py

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Homepage),
     path('html',views.HtmlViewer),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
